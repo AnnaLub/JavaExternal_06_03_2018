4bdf0831a5dbfad8f9119f1cba5d5f8d5dbcdaa1
AnnaLub <AnnaLub@users.noreply.github.com>
2018-04-26 22:01:37 +0300

    multythreading

---
+++ fastfood/src/main/java/AvailablePaydesk.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.Semaphore;
+
+public class AvailablePaydesk {
+    private static boolean [] AVAILABLE_PAYDESK = new boolean[7];
+    private static final Semaphore SEMAPHORE = new Semaphore(7,true);
+
+    public static void setAvailablePaydesk(int i, boolean flag) {
+        AVAILABLE_PAYDESK[i] = flag;
+    }
+
+    public static Semaphore getSEMAPHORE() {
+        return SEMAPHORE;
+    }
+
+    public static boolean getAvailablePaydesk(int i) {
+        return AVAILABLE_PAYDESK[i];
+    }
+
+    public static boolean[] getAvailablePaydesk() {
+        return AVAILABLE_PAYDESK;
+    }
+}
---
+++ fastfood/src/main/java/Visitor.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.Semaphore;
+
+public class Visitor implements Runnable{
+    private int visitorNumber;
+
+    public Visitor(int visitorNumber) {
+        this.visitorNumber = visitorNumber;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Visitor № " + visitorNumber + " entered McDonald's");
+        try
+        {
+        AvailablePaydesk.getSEMAPHORE().acquire();
+        int availablePaydesk = -1;
+        int threadPause = -1;
+            for (int i = 0; i < 7; i++)
+                if (!AvailablePaydesk.getAvailablePaydesk(i)) {
+                AvailablePaydesk.setAvailablePaydesk(i, true);
+                availablePaydesk = i;
+                threadPause = i;
+                System.out.println("Paydesk № " + availablePaydesk + " accepted order from Visitor № " + visitorNumber);
+                break;
+                }
+                if(threadPause%2!=0||threadPause==0){
+                Thread.sleep(4000);
+                }
+                else {
+                Thread.sleep(5100);
+                }
+                AvailablePaydesk.setAvailablePaydesk(availablePaydesk, false);
+                AvailablePaydesk.getSEMAPHORE().release();
+                System.out.println("Visitor № " + visitorNumber + " finished his order");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
